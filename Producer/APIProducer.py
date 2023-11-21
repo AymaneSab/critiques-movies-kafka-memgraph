7b1c62003270ea9b16b62122b0e68054ad89754e
AymaneSab <88624531+AymaneSab@users.noreply.github.com>
2023-11-21 11:34:53 +0100

    Add Data

--- Producer/APIProducer.py
+++ Producer/APIProducer.py
@@ -11,7 +11,6 @@ from datetime import datetime
 
 # Import Real-Time-Movie-Recommendation-System Folder Relative Path
 sys.path.append('/home/hadoop/Real-Time-Movie-Recommendation-System') 
-from API.Scrapper import get_movies
 
 # Set up Loggin Function
 def setup_producer_logging():
@@ -55,35 +54,23 @@ def produce_to_Topics(movieTopic, reviewTopic, producer_logger):
             movie_json = json.loads(movie_data)
 
             # Check if "release_date" is present and not null in the movie object
-            if movie_json["movie"]["release_date"] is not None:
-                try:
-                    # Format the release_date
-                    formatted_release_date = datetime.strptime(movie_json["movie"]["release_date"], "%Y-%m-%d").strftime("%Y-%m-%d")
-                    producer_logger.info(f"formatted_release_date :  {formatted_release_date} ")
+            try:
+                # Produce to the movies topic
+                producer.produce(topic, key="movie", value=json.dumps(movie_json["movie"]))
+                producer_logger.info(f"Movie Produced Successfully to {topic}: ")
 
-                    # Update the release_date field in the original message
-                    movie_json["movie"]["release_date"] = formatted_release_date
+                # Flush only if everything is successful
+                producer.flush()
 
-                    # Produce to the movies topic
-                    producer.produce(topic1, key="movie", value=json.dumps(movie_json["movie"]))
-                    producer_logger.info(f"Movie Produced Successfully to {topic1}: ")
+            except ValueError as ve:
+                # Log the error if the date formatting fails
+                error_message = f"Error formatting release date: {ve}"
+                producer_logger.error(error_message)
 
-                    # Produce to the reviews topic
-                    producer.produce(topic2, key="review", value=json.dumps(movie_json["review"]))
-                    producer_logger.info(f"Review Produced Successfully to {topic2}: ")
-
-                    # Flush only if everything is successful
-                    producer.flush()
-
-                except ValueError as ve:
-                    # Log the error if the date formatting fails
-                    error_message = f"Error formatting release date: {ve}"
-                    producer_logger.error(error_message)
-
-                except Exception as ex:
-                    # Log other validation errors
-                    error_message = f"Error validating Kafka message: {ex}"
-                    producer_logger.error(error_message)
+            except Exception as ex:
+                # Log other validation errors
+                error_message = f"Error validating Kafka message: {ex}"
+                producer_logger.error(error_message)
 
             # Add a delay to control the rate of data production
             time.sleep(2)
@@ -95,7 +82,7 @@ def produce_to_Topics(movieTopic, reviewTopic, producer_logger):
         error_message = "Error producing to Kafka: " + str(e)
         producer_logger.error(error_message)
 
-def runKafkaProducer(topic1, topic2):
+def runKafkaProducer(topic):
     
     producer_logger = setup_producer_logging()
 
@@ -106,13 +93,13 @@ def runKafkaProducer(topic1, topic2):
         admin_client = AdminClient({"bootstrap.servers": "localhost:9092"})
 
         # Check if the topics exist, and create them if not
-        for topic in [topic1, topic2]:
+        for topic in [topic]:
             existing_topics = admin_client.list_topics().topics
             if topic not in existing_topics:
                 create_kafka_topic(topic, admin_client, producer_logger)
 
         # Start producing to both topics simultaneously
-        produce_to_Topics(topic1 , topic2 , producer_logger)
+        produce_to_Topics(topic, producer_logger)
 
         
     except KeyboardInterrupt:
@@ -122,7 +109,6 @@ def runKafkaProducer(topic1, topic2):
         error_message = "An unexpected error occurred in Kafka Producer: " + str(e)
         producer_logger.error(error_message)
 
-topic1 = "Movies"
-topic2 = "Reviews"
+topic = "Reviews"
 
-runKafkaProducer(topic1, topic2)
+runKafkaProducer(topic)
